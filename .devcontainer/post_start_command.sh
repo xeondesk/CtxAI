07b1b06330a5cb5445ef746ecf7e3b24f692982f
xeondesk <xeondesk@gmail.com>
2026-03-07 14:29:11 +0000

    update

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+WORKSPACE_ROOT=$(pwd)
 
-pip install -r requirements.txt
+pipx install uv
+uv sync
+
+echo "alias start-worker=\"cd $WORKSPACE_ROOT && uv run python -m celery -A app.celery worker -P threads -c 1 --loglevel INFO -Q dataset,dataset_summary,priority_dataset,priority_pipeline,pipeline,mail,ops_trace,app_deletion,plugin,workflow_storage,conversation,workflow,schedule_poller,schedule_executor,triggered_workflow_dispatcher,trigger_refresh_executor,retention\"" >> ~/.bashrc
+
+source /home/vscode/.bashrc
